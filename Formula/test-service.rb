bcbd10784b95d94a374849982c2b086d8f2362d7
Pankaj Kargirwar <kargirwar@gmail.com>
2021-03-29 20:13:58 +0530

    Checking OS

--- Formula/test-service.rb
+++ Formula/test-service.rb
@@ -1,12 +1,15 @@
 class TestService < Formula
   desc "This service has been created to test brew"
   homepage "https://dev0.kargirwar.com"
-  url "https://break-timer.kargirwar.com/release.zip"
   version "1.0"
-  sha256 "04acf3831ba06a4275dea71b3dfc039ea5a3b99345ecae014d4d773928450b65"
   license "MIT"
 
   bottle :unneeded
+
+  if os.Mac? && Hardware::CPU.intel?
+    url "https://break-timer.kargirwar.com/release.zip"
+    sha256 "04acf3831ba06a4275dea71b3dfc039ea5a3b99345ecae014d4d773928450b65"
+  end
 
   def install
     bin.install "test-service"
