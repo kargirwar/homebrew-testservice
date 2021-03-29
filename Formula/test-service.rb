dd7f80da066ed4553f1b01f4ca2bf76e9f8e8bf4
Pankaj Kargirwar <kargirwar@gmail.com>
2021-03-29 18:32:44 +0530

    Using precompiled binary

--- Formula/test-service.rb
+++ Formula/test-service.rb
@@ -3,15 +3,10 @@ class TestService < Formula
   homepage "https://dev0.kargirwar.com"
   url "https://break-timer.kargirwar.com/release.zip"
   version "1.0"
-  sha256 "0d624efb15c52b6a3bcc02fb2ee13f753d052556ba1b8ff91f5a6c737fec59fe"
+  sha256 "04acf3831ba06a4275dea71b3dfc039ea5a3b99345ecae014d4d773928450b65"
   license "MIT"
 
-  depends_on "go" => :build
-
-  def install
-    system "go", "build", *std_go_args
-  end
-
+  bottle :unneeded
   test do
     assert_match "Version 1.0", shell_output(bin/"test-service -v")
   end
