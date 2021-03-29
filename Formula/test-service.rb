36d29cc659a63995a19c5090dbaefa9663f9c945
Pankaj Kargirwar <kargirwar@gmail.com>
2021-03-29 17:52:20 +0530

    Updated SHA

--- Formula/test-service.rb
+++ Formula/test-service.rb
@@ -3,7 +3,7 @@ class TestService < Formula
   homepage "https://dev0.kargirwar.com"
   url "https://break-timer.kargirwar.com/release.zip"
   version "1.0"
-  sha256 "99a647c5cc798134c1057bfe7f36d06e6f48c983a862b49dffbe377236c81b8a"
+  sha256 "0d624efb15c52b6a3bcc02fb2ee13f753d052556ba1b8ff91f5a6c737fec59fe"
   license "MIT"
 
   depends_on "go" => :build
