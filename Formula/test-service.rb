e063ad3a608c013b671d59fda18d1385f3fdeafc
Pankaj Kargirwar <kargirwar@gmail.com>
2021-03-29 16:08:42 +0530

    Updated formula

--- Formula/test-service.rb
+++ Formula/test-service.rb
@@ -2,12 +2,10 @@ class TestService < Formula
   desc "This service has been created to test brew"
   homepage "https://dev0.kargirwar.com"
   url "git@github.com:kargirwar/test-service.git"
-  ver "1.0"
 
   depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "go", "build", *std_go_args
   end
 
